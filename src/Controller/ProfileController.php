05c95af0fc0be3e3dc675eeebe98d90bce7f8779
jo0ker64 <martel.jonathan64@gmail.com>
2024-07-28 19:11:30 +0200

    maj

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProfileController extends AbstractController
+{
+    // Route pour la page de profil
+    #[Route('/profile', name: 'app_profile')]
+    public function index(Request $request): Response
+    {
+        $user = $this->getUser(); // Récupération de l'utilisateur connecté
+
+        if (!$user) {
+            throw $this->createNotFoundException('Vous devez être connecté pour accéder à cette page');
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]); // Rendu de la vue de profil avec les données de l'utilisateur      
+    }
+}
